7a15a891cd237ba292b8b7c8c117ead5f45d44cc
Shaurya <as.shauryagupta@gmail.com>
2022-11-28 14:57:39 +0530

    three pointer approach Sortingsenior.java

--- src/Sortingsenior.java
+++ src/Sortingsenior.java
@@ -3,35 +3,32 @@ import java.util.ArrayList;
 public class Sortingsenior {
     public static void main(String[] args) {
 //    HashMap<Integer,Integer> hm=new HashMap<>();
-    ArrayList<Integer> res=new ArrayList<>();
-    int arr[]={2,0,2,1,1,0};
-    int i,a=0,b=0,c=0,flag=0;
-
-    for(i=0;i<arr.length;i++)
-    {
-        if(arr[i]==0)
-            a+=1;
-        if(arr[i]==1)
-            b+=1;
-        if(arr[i]==2)
-            c+=1;
-    }
-    for(i=1;i<=a;i++){
-        arr[flag]=0;
-        flag+=1;
-    }
-        for(i=1;i<=b;i++){
-            arr[flag]=1;
-            flag+=1;
+        ArrayList<Integer> res = new ArrayList<>();
+        int arr[] = {2, 0, 2, 1, 1, 0};
+        int i, a = 0, b = 0, c = 0, flag = 0, temp;
+        int two = arr.length - 1, current = 0, zero = 0;
+        while (current <= two) {
+            if (arr[current] == 0) {
+                temp = arr[current];
+                arr[current] = arr[zero];
+                arr[zero] = temp;
+                zero += 1;
+                current += 1;
+            } else if (arr[current] == 2) {
+                temp = arr[current];
+                arr[current] = arr[two];
+                arr[two] = temp;
+                two -= 1;
+                current += 1;
+            } else {
+                current += 1;
+            }
         }
-        for(i=1;i<=c;i++){
-            arr[flag]=2;
-            flag+=1;
-        }
-     for(i=0;i<arr.length;i++)
-     System.out.print(arr[i]+",");
+        for(i=0;i<arr.length;i++)
+            System.out.print(arr[i]+",");
 
     }
 }
-//2 0 1 2 1 1 0 1
-//Dutch National
+
+
+
