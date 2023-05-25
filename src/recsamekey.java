f96e0963502785eeb689ca74f8f8b05a08c2e7d8
Shaurya <as.shauryagupta@gmail.com>
2023-05-25 14:09:04 +0530

    first index having the same key of an array

---
+++ src/recsamekey.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class recsamekey {
+    public static int samekey(int arr[],int ind,int key ){
+        if(ind==arr.length){
+            return -1;
+        }
+        if (arr[ind] == key) {
+            return ind;
+        }
+        else{
+            return samekey(arr,ind+1,key);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter the length of array");
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("enter array elements");
+            arr[i]=in.nextInt();
+        }
+        System.out.println("enter the value to be checked");
+        int check=in.nextInt();
+        System.out.println("the index is :"+samekey(arr,0,check));
+
+
+    }
+}
