24caa34cb86a1acf923348fa1eecb649c688d7e4
Shaurya <as.shauryagupta@gmail.com>
2023-05-25 09:40:35 +0530

    given a number n , print n-1

---
+++ src/printn2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class printn2 {
+    public static void print(int n){
+        if(n==0){
+            return ;
+        }
+        System.out.print(n + " ");
+        print(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=in.nextInt();
+        print(n);
+    }
+}
